664c6c89e27d7c23671cc5dd7d76059e58c5907c
Matias Aguilera <aguileramati50@gmail.com>
2021-01-25 16:04:10 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
+const ContainerApp = styled.div`
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContainerApp className="container-fluid">
+      <div className="row">
+        <div className="navbar navbar-light bg-light">
+          <span className="navbar-brand ms-5" >Personal List</span>
+        </div>
+        <div className="col-12">
+
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </ContainerApp>
+
   );
 }
 
